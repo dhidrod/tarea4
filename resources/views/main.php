ab3c8cbacdee79fb926c0c0196a60a32dc9c97b3
dhidrod <adanielhidalgo@gmail.com>
2025-04-22 18:55:11 +0200

    Modificada la página principal que ve el usuario al estar en inicio una vez logeado.

--- resources/views/main.php
+++ resources/views/main.php
@@ -1,4 +1,33 @@
-<h1 class="centered">Agregar Nuevo Partido</h1>
-<main class="cuerpoformulario">
-    
-</main>
+<div class="container">
+    <h1>🎬 Cartelera de Cine</h1>
+    <div class="cartelera">
+
+      <!-- Película 1 -->
+      <div class="pelicula">
+        <img src="images/peli1.jpg" alt="Los Vengadores - Infinity War">
+        <div class="info">
+          <h2>Los Vengadores - Infinity War</h2>
+          <p>Los Vengadores y sus aliados deben estar dispuestos a sacrificarlo todo para derrotar al poderoso Thanos antes de que su ataque devastador acabe con la humanidad y el universo. Género: Acción, Aventura, Ciencia ficción. Duración: 149 minutos. Clasificación: Apta para mayores de 12 años.</p>
+        </div>
+      </div>
+
+      <!-- Película 2 -->
+      <div class="pelicula">
+        <img src="images/peli2.jfif" alt="Star Wars - El Imperio Contraataca">
+        <div class="info">
+          <h2>Star Wars - El Imperio Contraataca</h2>
+          <p>La galaxia está al borde de la destrucción. Luke Skywalker, un joven granjero, se une a una valiente banda de rebeldes para luchar contra el malvado Imperio Gal ctico y su oscuro Lord, Darth Vader. Género: Ciencia ficción, Acción, Aventura. Duración: 124 minutos. Clasificación: Apta para todos los públicos.</p>
+        </div>
+      </div>
+
+      <!-- Película 3 -->
+      <div class="pelicula">
+        <img src="images/peli3.jpg" alt="JAWS - La Película">
+        <div class="info">
+          <h2>JAWS - La Película</h2>
+          <p>Un gran tiburón blanco aterroriza a una pequeña ciudad costera. Un policía, un biólogo marino y un cazador de tiburones se unen para cazarlo. Género: Terror, Suspenso. Duración: 124 minutos. Clasificación: Apta para mayores de 13 años.</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
